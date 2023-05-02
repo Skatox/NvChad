2e9dae6268a8c7e0fdb8b88c9676ca6759a52f78
skatox <migueluseche@skatox.com>
2023-05-02 14:37:14 -0500

    feat: tabnine completion with s-tab

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -29,7 +29,7 @@ local plugins = {
     config = function()
       require('tabnine').setup({
          disable_auto_comment=true,
-         accept_keymap="<Tab>",
+         accept_keymap="<S-Tab>",
          dismiss_keymap = "<C-]>",
          debounce_ms = 800,
          suggestion_color = {gui = "#808080", cterm = 244},
